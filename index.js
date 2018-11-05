dc0500a6f833747d740555833f34d203b463a7ee
Hector Ariceaga <hector_ariceaga@hotmail.com>
2018-11-05 20:33:55 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
 // Write your code in this file!
 function scuberGreetingForFeet(someValue) {
-  
+  let message
+  if (someValue > 0 && someValue <= 400) {
+    message = "This one is on me!"
+  }
+  return message
 }
